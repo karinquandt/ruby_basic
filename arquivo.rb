885b801c946833325abab05ccc6a646b3fcb2fea
Karin Quandt <jaquempo@gmail.com>
2017-12-02 09:52:12 -0200

    Arquivo com loop do curso de automacao

---
+++ arquivo.rb
@@ -0,0 +1,16 @@
+variavel_inteiro = 3
+variavel_boolean = true
+variavel_string = "karin"
+
+m = 0
+loop do
+    m += 1
+    puts "Quero imprimir a variável inteira: #{variavel_inteiro}"
+    puts "Quero imprimir a variável boolean: #{variavel_boolean}"
+    puts "Quero imprimir a variável string: #{variavel_string}"
+    puts "------------------------------------------------"
+    break if m == 5
+
+end
+
+
